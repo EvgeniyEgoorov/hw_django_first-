b5fc6b8c1b35c585a39455520e323bc4e9669a59
Egorov Evgeniy <egorovzhenya@gmail.com>
2022-04-03 11:21:43 +0400

    Pagination bug is fixed

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -4,22 +4,21 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 import csv
 
+with open(settings.BUS_STATION_CSV, encoding='Utf-8', newline='') as f:
+    content = list(csv.DictReader(f))
+
 
 def index(request):
     return redirect(reverse('bus_stations'))
 
 
 def bus_stations(request):
-
-    with open(settings.BUS_STATION_CSV, encoding='Utf-8', newline='') as f:
-        content = list(csv.DictReader(f))
-        page_number = int(request.GET.get('page', 1))
-        paginator = Paginator(content, 10)
-        page = paginator.get_page(page_number)
-        page_content = page.object_list
-
-        context = {
-            'bus_stations': page_content,
-            'page': page
-        }
-        return render(request, 'stations/index.html', context)
+    page_number = int(request.GET.get('page', 1))
+    paginator = Paginator(content, 10)
+    page = paginator.get_page(page_number)
+    page_content = page.object_list
+    context = {
+        'bus_stations': page_content,
+        'page': page
+    }
+    return render(request, 'stations/index.html', context)
